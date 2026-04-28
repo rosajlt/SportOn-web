f93894d45fe19fd89e3d48ba109059baf9ac216f
rosajlt <rosaanjelita23@gmail.com>
2026-04-28 11:39:34 +0700

    add middleware for request handling and update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,4 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export const config = {
-  matcher: [
-    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|.*\\.[^/]+$).*)",
-  ],
-}
-
 export default nextConfig;
